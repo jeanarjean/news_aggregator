055187ca8059351f301de806ed2903db3194c573
Jeremie Roy <j-roy95@hotmail.com>
2018-01-27 16:17:46 -0600

    Add query to search bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,47 +4,68 @@ import Article from  './Article';
 import './App.css';
 
 class App extends Component {
-    state = {users: []}
+  
+  
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const queryValue = document.getElementById('searchQuery').value;
+    fetch(`/query?q=${queryValue}`, {method: 'GET'})
+    .then(res=>res.json())
+    .then(res=>console.log(res));
+    return false;
+  }
+  state = {users: []}
 
-    componentDidMount() {
-        fetch('/users')
-            .then(res => res.json())
-            .then(users => this.setState({ users }))
-            .then(console.log(this.state));
-    }
+  componentDidMount() {
+      fetch('/users')
+          .then(res => res.json())
+          .then(users => this.setState({ users }))
+          .then(console.log(this.state));
+  }
 
   render() {
     return (
       <div className="App">
         <div className="container">
-        <form id="searchForm">
-          <div className="row">
-            <div className="col-3"></div>
-            <div className="col-6">
-            <FormGroup>
-            <Label for="searchQuery">Search something</Label>
-            <Input className="search" type="search" name="search" id="searchQuery" placeholder="Search for something" />
-            </FormGroup>
-            </div>
-            <div className="col-3"></div>
-          </div>
-          <div className="row">
-            <div className="col-3"></div>
-            <div className="col-6">
+
+        <SearchBar handleSubmit={this.handleSubmit}/>
+
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-8">
               <Article/>
               <Article/>
-            </div>
-            <div className="col-3"></div>
           </div>
-        </form>
+          <div className="col-2"></div>
+          </div>
+          
         </div>
-        
       </div>
     );
   }
 }
 
-
+const SearchBar = (props) => {
+  return (
+    <form id="searchForm" onSubmit={props.handleSubmit}>
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6">
+        <FormGroup>
+        <Label for="searchQuery">Search for anything</Label>
+        <Input className="search" type="search" name="search" id="searchQuery" placeholder="Search for something" />
+        </FormGroup>
+        </div>
+        <div className="col-3"></div>
+      </div>
+    </form>
+  );
+}
 
 
 export default App;
--- client/src/Article.js
+++ client/src/Article.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle,
-    CardSubtitle, Button } from 'reactstrap';
+    CardSubtitle } from 'reactstrap';
 
 class Article extends Component {
     render() {
@@ -11,7 +11,6 @@ class Article extends Component {
                     <CardTitle>Card title</CardTitle>
                     <CardSubtitle>Card subtitle</CardSubtitle>
                     <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <Button>Button</Button>
                     </CardBody>
                 </Card>
             </div>

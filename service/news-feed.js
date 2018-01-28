4959610df707fe33aecef2d55bac20fdac26d23a
David Dallaire <ddallaire25@gmail.com>
2018-01-27 19:00:47 -0500

    Fix indentation and remove blankspaces

--- service/news-feed.js
+++ service/news-feed.js
@@ -1,48 +1,40 @@
-
-
 const NewsAPI = require('newsapi');
 const extractor = require('unfluff');
 const request = require("request");
 const async = require('async');
 const newsapi = new NewsAPI('acb7dd9b7f5b40e5ae90aaea2b195c01');
 
-
 module.exports = {
-   
-    getNews: function(keyword,callback) {
-      var numNewsResult
-      var news = []
-      newsapi.v2.everything({   
-        q: keyword,
-        language: 'en',
-      }).then(response => {
-        if(response.status == "ok"){
-          numNewsResult = response.totalResults
-
-          async.each(response.articles, function (item, callback){ 
-            getFullText(item.url, function( error, fullText){
-            var article = {
-              title: item.title,
-              image: item.urlToImage,
-              newsText: fullText,
-              url: item.url
-            };
-            news.push(article);
-            callback();
-          });
-        }, function(err) {
-          callback(err, news);
+  getNews: function(keyword,callback) {
+    var news = []
+    newsapi.v2.everything({   
+      q: keyword,
+      language: 'en',
+    }).then(response => {
+      if(response.status == "ok"){
+        async.each(response.articles, function (item, callback){ 
+          getFullText(item.url, function( error, fullText){
+          var article = {
+            title: item.title,
+            image: item.urlToImage,
+            newsText: fullText,
+            url: item.url
+          };
+          news.push(article);
+          callback();
         });
-        }
+      }, function(err) {
+        callback(err, news);
       });
-    }    
-  };
-
-  function getFullText(url, callback){
-    request.get( url, function(error, response, body){
-      var text = extractor(body, 'en').text
-      text = text.replace(/\n/g,' ').replace('  ', ' ');
-      callback(error, text);
+      }
     });
-  }
+  }    
+};
 
+function getFullText(url, callback){
+  request.get( url, function(error, response, body){
+    var text = extractor(body, 'en').text
+    text = text.replace(/\n/g,' ').replace('  ', ' ');
+    callback(error, text);
+  });
+}

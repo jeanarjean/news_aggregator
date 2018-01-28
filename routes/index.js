241bcf24360a07afceda014f8189924f421a189a
Jeremie Roy <j-roy95@hotmail.com>
2018-01-28 09:24:39 -0600

    Make prod deployment

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@ var textToSpeech = require('../service/text-to-speech');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var sampleText = 'Bitcoin is a cryptocurrency and worldwide payment system. It is the first decentralized digital currency, as the system works without a central bank or single administrator.';
-  textToSpeech.createWav(sampleText, function(err, file) {
-    res.render('index', { title: JSON.stringify(file) });
-  });
+ res.sendFile('./public/index.html');
 });
 
 module.exports = router;
